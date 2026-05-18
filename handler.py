ed0ab61dbf5b408bea72d92660f914ef3db464cc
Matas <matasrevinskas@gmail.com>
2026-05-18 14:14:46 +0300

    Add tyro to bootstrap deps (required by gsplat simple_trainer)

--- handler.py
+++ handler.py
@@ -45,7 +45,7 @@ def do_bootstrap():
                 ensure(["apt-get", "update", "-qq"], "apt update")
                 ensure(["apt-get", "install", "-y", "-qq", pkg], f"apt install {pkg}")
 
-        for dep in ["jaxtyping", "gsplat", "nerfview", "viser", "opencv-python-headless", "plyfile"]:
+        for dep in ["jaxtyping", "gsplat", "nerfview", "viser", "opencv-python-headless", "plyfile", "tyro"]:
             try:
                 __import__(dep.replace("-", "_"))
             except ImportError:
